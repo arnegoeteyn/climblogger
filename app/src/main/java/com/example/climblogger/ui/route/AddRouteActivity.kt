c7b432c8cfe5059acc63dd1a4b33c5d92268da68
Arne Goeteyn <goeteynarne@gmail.com>
2019-06-25 14:24:38 +0200

    all fields in route except sector

--- app/src/main/java/com/example/climblogger/ui/route/AddRouteActivity.kt
+++ app/src/main/java/com/example/climblogger/ui/route/AddRouteActivity.kt
@@ -1,12 +1,12 @@
 package com.example.climblogger.ui.route
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import com.example.climblogger.R
 import com.example.climblogger.data.Route
 import kotlinx.android.synthetic.main.activity_add_route.*
-import kotlinx.android.synthetic.main.activity_route.*
 import java.util.*
 
 class AddRouteActivity : AppCompatActivity() {
@@ -20,17 +20,32 @@ class AddRouteActivity : AppCompatActivity() {
         addRouteViewModel = ViewModelProviders.of(this).get(AddRouteViewModel::class.java)
 
         addRouteButton.setOnClickListener { addRoute() }
+
+        initKindSpinner()
     }
 
     private fun addRoute() {
+        val commentText = commentTextInput.editText!!.text.toString()
+        val linkText = linkTextInput.editText!!.text.toString()
         addRouteViewModel.insertRoute(
             Route(
-                0, nameTextInput.editText!!.text.toString(),
-                "8a", "sport", null, null,
+                0,
+                nameTextInput.editText!!.text.toString(),
+                gradeTextInput.editText!!.text.toString(),
+                kindSpinner.selectedItem as String,
+                if (commentText.isEmpty()) null else commentText,
+                if (linkText.isEmpty()) null else linkText,
                 null, null,
                 UUID.randomUUID().toString()
             )
         )
         finish()
+    }
+
+    private fun initKindSpinner() {
+        val arrayAdapter =
+            ArrayAdapter.createFromResource(this, R.array.route_kind, android.R.layout.simple_spinner_item)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        kindSpinner.adapter = arrayAdapter
     }
 }
