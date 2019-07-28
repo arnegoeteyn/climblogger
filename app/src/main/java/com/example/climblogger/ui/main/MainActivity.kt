f47f6c5c24af86083426c6182ba17a0002d063a9
Arne Goeteyn <goeteynarne@gmail.com>
2019-07-28 12:50:00 +0200

    add toolbar

--- app/src/main/java/com/example/climblogger/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/climblogger/ui/main/MainActivity.kt
@@ -32,6 +32,8 @@ class MainActivity : AppCompatActivity(),
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+
         initBottomNavigation()
 
         switchToRoutes()
---
+++ app/src/main/java/com/example/climblogger/ui/stats/StatsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.climblogger.ui.stats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.climblogger.R
+
+class StatsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stats)
+    }
+}
