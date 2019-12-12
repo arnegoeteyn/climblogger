6f4e003be5f4954b2e82f3fb65c1da2e0e04fd89
Arne Goeteyn <goeteynarne@gmail.com>
2019-12-12 09:25:28 +0100

    correct ordering

--- app/src/main/java/com/example/climblogger/data/RouteWithAscents.kt
+++ app/src/main/java/com/example/climblogger/data/RouteWithAscents.kt
@@ -28,6 +28,7 @@ interface RouteWithAscentsDoa {
         """
                 select name, grade, routes.kind, route_uuid, count(name) as amount from routes inner join ascents USING(route_uuid)
                 group by name
+                ORDER BY grade desc, name asc
             """
     )
     fun getRoutesWithAscents(): LiveData<List<RouteWithAscents>>
@@ -37,6 +38,7 @@ interface RouteWithAscentsDoa {
                 select name, grade, routes.kind, route_uuid, count(name) as amount from routes inner join ascents USING(route_uuid)
                 where sector_uuid == :sector_id
                 group by name
+                ORDER BY grade desc, name asc
             """
     )
     fun routesFromSector(sector_id: String): LiveData<List<RouteWithAscents>>
@@ -44,8 +46,8 @@ interface RouteWithAscentsDoa {
 
 
 class RouteWithAscentsRepository(private val routeWithAscentsDoa: RouteWithAscentsDoa) {
-    val routeWithAscents: LiveData<List<RouteWithAscents>> = routeWithAscentsDoa.getRoutesWithAscents()
 
+    val routeWithAscents: LiveData<List<RouteWithAscents>> = routeWithAscentsDoa.getRoutesWithAscents()
 
     fun routesFromSector(sector_id: String): LiveData<List<RouteWithAscents>> {
         return routeWithAscentsDoa.routesFromSector(sector_id)
