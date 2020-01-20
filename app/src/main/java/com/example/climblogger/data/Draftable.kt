6b1dc5942a494573e8f9fcc7a8337f1be31bf78e
Arne Goeteyn <goeteynarne@gmail.com>
2020-01-20 17:27:46 +0100

    mend

--- app/src/main/java/com/example/climblogger/data/Draftable.kt
+++ app/src/main/java/com/example/climblogger/data/Draftable.kt
@@ -7,7 +7,7 @@ abstract class Draftable<Me : Draftable<Me>> {
 
     abstract class Draft<Me : Draftable<Me>> {
         fun fromDraft(): Me? {
-            if (this.javaClass.declaredFields.all {
+            if (javaClass.fields.all {
                     // check that the value isn't null or that we allow it to be null
                     it.get(this) != null || it.isAnnotationPresent(NullableOutDraft::class.java)
                 }) {
